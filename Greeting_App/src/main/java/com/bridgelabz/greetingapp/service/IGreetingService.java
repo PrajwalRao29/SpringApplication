d1d77c0cd139b03edb90af73b98cfb43639e62e7
PrajwalRao29 <raoprajrao@gmail.com>
2020-11-26 19:38:28 +0530

    Simple greeting app

---
+++ Greeting_App/src/main/java/com/bridgelabz/greetingapp/service/IGreetingService.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.greetingapp.service;
+
+import com.bridgelabz.greetingapp.model.Greeting;
+import com.bridgelabz.greetingapp.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IGreetingService {
+    Greeting addGreeting(User user);
+    Optional<Greeting> getGreetingById(long id);
+
+    List<Greeting> getAllGreetings();
+
+    Greeting updateGreeting(long id, String message);
+
+    void deleteGreeting(long id);
+}
